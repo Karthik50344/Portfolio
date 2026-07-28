bfe7abc7ef00bb19212fc662272e7ed4485fd189
Karthik50344 <karthikeyan08082003@gmail.com>
2026-07-28 14:43:53 +0530

    Changes: 1. Implemented product and product details screen for show affliated program products 2. added product.json, product model for fetch and handle data 3. Implemented affiliated disclosure, privacy policy, and terms of use

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"d3d45dcf251823c1769909cd43698d126db38d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2889405505"
+    serviceWorkerVersion: "2876424655"
   }
 });
